8cedcb9b8abd5d540c423b7055bdcd5a3abfb513
Roswell Doria <rdoria6@my.bcit.ca>
2021-05-05 14:27:37 -0700

    init vue CLI

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- Route your views here-->
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <!-- Display compoents here -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import components you wish to display in this view
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //Declair components down bellow
+    //HelloWorld
+  }
+}
+</script>
