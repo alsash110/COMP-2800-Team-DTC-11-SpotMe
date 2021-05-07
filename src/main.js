2fdad6aefc93decf2691f029f6cfb208faec9f8d
Roswell Doria <rdoria6@my.bcit.ca>
2021-05-06 21:39:24 -0700

    Install firebase/firestore using npm

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBfmIAGUvMVLnGM_FV_dthp-z7p7XWAopo",
+  authDomain: "group11-spot-me.firebaseapp.com",
+  projectId: "group11-spot-me",
+  storageBucket: "group11-spot-me.appspot.com",
+  messagingSenderId: "1023138880515",
+  appId: "1:1023138880515:web:c5674a532cd90b41a30236"
+})
+
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
