b7df3f274210ce799526015973347e5e9c98c269
pidgeonforlife <eric.choiyongseok@gmail.com>
2021-05-14 02:40:20 -0700

    Implemented a homepage for the application.

---
+++ src/components/HomePageCarousel.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+<script>
+    export default {
+    data () {
+      return {
+        items: [
+          {
+            src: "https://firebasestorage.googleapis.com/v0/b/group11-spot-me.appspot.com/o/Images%2FGirls%20Running.png?alt=media&token=b9e7d7a5-a3db-401e-bff7-601abc6bcb14",
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
+</script>
