1cdacb6b83ba58a73e78cf309df462d04c8d7bcd
alsash110 <71096449+alsash110@users.noreply.github.com>
2021-05-18 23:18:14 -0700

    First commit. Router issues unresolved

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,71 +1,117 @@
 <template>
 <div>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
   
+  <validation-observer
+    ref="observer"
+    v-slot="{ invalid }"
+  >
+    <form @submit.prevent="submit">
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+<v-container fluid id="box">
+<validation-provider
+        v-slot="{ errors }"
+        name="email"
+        rules="required|email"
+      >
+        <v-text-field
+          v-model="email"
+          :error-messages="errors"
+          label="E-mail"
+          required
+        ></v-text-field>
+      </validation-provider>
 
-     <v-text-field
-      v-model="password"
-      :type="passwor"
-      :rules="nameRules"
-      label="Password"
-      required
-    ></v-text-field>
+      <validation-provider
+        v-slot="{ errors }"
+        name="Password"
+        rules="required"
+      >
+        <v-text-field
+          v-model="password"
+          type="password"
+          :error-messages="errors"
+          label="Password"
+          required
+        ></v-text-field>
+      </validation-provider>
+      
+            <v-btn
+        class="mr-4"
+        type="submit"
+        color="primary"
+        :disabled="invalid"
+      >
+        submit
+      </v-btn>
+      <v-btn
+      color="success"
+      to="/signup"
 
-    <v-btn
-    :disabled="!valid"
-      color="primary"
-      class="mr-4"
-      @click="validate"
-      to="/displaymatches"
-    >
-      Login
-    </v-btn>
-
-    <v-btn
-      color="teal"
-      class="mr-4"
-      to="/Signup"
-    >
-      Signup
-    </v-btn>
-
-  </v-form>
-
-   <v-footer height="80px" class="blue-rectangle" color="#97CAFA" fixed>
+      >
+        Signup
+      </v-btn>
+   </v-container >
+    </form>
+    
+ </validation-observer>
+ 
+<v-footer height="80px" class="blue-rectangle" color="#97CAFA" fixed>
         </v-footer>
           </div>
+
 </template>
 
 <script>
+  import { required, email } from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+  import * as firebase from "firebase/app"
+
+
+ 
+  setInteractionMode('eager')
+
+  extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+
+  extend('email', {
+    ...email,
+    message: 'Email must be valid',
+  })
+
   export default {
+    components: {
+      ValidationProvider,
+      ValidationObserver,
+    },
     data: () => ({
-      valid: false,
       password: '',
-      nameRules: [
-        v => !!v || 'Password is required'
-      ],
       email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ]
     }),
 
     methods: {
-      validate () {
-        this.$refs.form.validate()
+     async submit () {
+        this.$refs.observer.validate()
+        try{
+          const val = await firebase
+          console.log(val);
+          this.$router.repalce({name:"mainsettings"});
+        }
+        catch(err){
+          console.log(err);
+        }
+        
       }
     },
   }
 </script>
+
+<style scoped>
+
+  #box{
+    margin: auto;
+    width: 80vw;
+    justify-content: center;
+      }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import About from '../views/About.vue'
 import Home from '../views/Home.vue'
 import Chat from '../views/Chat.vue'
 import Signup from '../views/Signup.vue'
@@ -9,6 +10,10 @@ import DisplayMatches from '../views/DisplayMatches.vue'
 import mainsettings from '../views/mainsettings.vue'
 import Settings from '../views/SettingsTwo.vue'
 import EditInfo from '../views/EditInfo.vue'
+
+import * as firebase from "firebase/app"
+import "firebase/auth"
+
 
 Vue.use(VueRouter)
 
@@ -21,51 +26,54 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   },
   {
     path: '/chat',
     name: 'Chat',
-    component: Chat
+    component: Chat,
+    meta: {requiresAuth: true}
   },
   {
     path: '/message/:id',
     name: 'Message',
     component: Message,
     props: true,
+    meta: {requiresAuth: true}
   },
   {
     path: '/displayMatches',
     name: 'DisplayMatches',
-    component: DisplayMatches
+    component: DisplayMatches,
+    meta: {requiresAuth: true}
   },
   {
     path: '/mainsettings',
     name: 'mainsettings',
-    component: mainsettings
+    component: mainsettings,
+    meta: {requiresAuth: true}
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+    component: Signup
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: Login
   },
   {
     path: '/settings',
     name: 'settings',
-    component: Settings
+    component: Settings,
+    meta: {requiresAuth: true}
   },
   {
     path: '/editinfo',
     name: 'editinfo',
-    component: EditInfo
+    component: EditInfo,
+    meta: {requiresAuth: true}
   }
 ]
 
@@ -73,5 +81,17 @@ const router = new VueRouter({
   routes
 })
 
+// THIS BREAKS EVERYTHING
+// router.beforeEach((to, from, next) => {
+//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+//   const isAuthenticated = firebase.auth().currentUser;
+//   console.log("isauthenticated", isAuthenticated);
+//   if (requiresAuth && !isAuthenticated) {
+//     next("/login");
+//   } else {
+//     next();
+//   }
+// });
+
+
 export default router
-// asn
