2faa9eac65e6c5146f03b783531e116391a87633
Justin Yoon <justinyoon23@gmail.com>
2021-05-26 05:50:15 -0700

    added social media sharing of app

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import VueSocialSharing from 'vue-social-sharing';
 import ScrollDiv from 'vue-scroll-div';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// library.add(faUserSecret)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 firebase.initializeApp({
   apiKey: "AIzaSyBfmIAGUvMVLnGM_FV_dthp-z7p7XWAopo",
